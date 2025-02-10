da5d97e3588c312403c4a7ad6dec4d7120420227
Ryan Furrer <ryandotfurrer@gmail.com>
2025-02-09 19:28:02 -0500

    fix shrinking checkbox issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
   const [tasks, setTasks] = useState<string[]>([])
   const [task, setTask] = useState<string>('')
   const [completedTasks, setCompletedTasks] = useState<Set<string>>(new Set())
+  const [tags, setTags] = useState<Tag[]>([])
 
   useEffect(() => {
     const savedTasks = sessionStorage.getItem('tasks')
@@ -40,11 +41,11 @@ export default function Home() {
     setTask('')
   }
 
-  const handleDelete = (index: number) => {
+  const deleteTask = (index: number) => {
     setTasks(tasks.filter((_, i) => i !== index))
   }
 
-  const handleComplete = (taskKey: string) => {
+  const completeTask = (taskKey: string) => {
     setCompletedTasks((prev) => {
       const newCompleted = new Set(prev)
       if (newCompleted.has(taskKey)) {
@@ -67,8 +68,8 @@ export default function Home() {
           <p className="text-xl">a simple task app</p>
         </hgroup>
       </header>
-      <main className="space-y-12">
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+      <main className="space-y-1">
+        <form className="mb-12 flex flex-col gap-4" onSubmit={handleSubmit}>
           <Input
             type="text"
             placeholder="add a task"
@@ -80,49 +81,57 @@ export default function Home() {
           </Button>
         </form>
         <div className="border-t py-4">
-          <ul className="space-y-1">
+          <ul className="space-y-4">
             {tasks.map((task) => (
               <div
                 key={task}
-                className="group -ml-2 flex items-center rounded-md border border-transparent p-2 transition-all hover:border-border hover:bg-card"
+                className="group gap-4 rounded-md border border-transparent p-2 transition-all hover:border-border hover:bg-card"
               >
-                <div className="mr-auto flex items-center gap-2">
+                <div className="flex items-center gap-4">
                   <input
                     type="checkbox"
-                    className="cursor-pointer accent-accent opacity-0 transition-opacity checked:bg-accent group-hover:opacity-100"
+                    className="size-4 cursor-pointer justify-center accent-accent opacity-0 transition-opacity checked:bg-accent group-hover:opacity-100"
                     name=""
                     id=""
                     checked={completedTasks.has(task)}
-                    onChange={() => handleComplete(task)}
+                    onChange={() => completeTask(task)}
                   />
                   <li
                     className={cn(
                       completedTasks.has(task)
                         ? 'text-foreground-muted line-through'
                         : 'text-foreground',
-                      '-translate-x-4 cursor-pointer transition-transform group-hover:translate-x-0',
+                      'w-full -translate-x-8 cursor-pointer transition-transform group-hover:translate-x-0',
                     )}
                     key={task}
-                    onClick={() => handleComplete(task)}
+                    onClick={() => completeTask(task)}
                   >
                     {task}
                   </li>
                 </div>
-                <div className="flex translate-x-4 items-center gap-2 opacity-0 transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+                <div className="mt-4 flex translate-y-4 items-center gap-2 opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+                  <div
+                    id="tags-container"
+                    className="mr-auto flex gap-2 text-xs"
+                  >
+                    <p className="rounded-sm bg-muted p-1">tag</p>
+                    <p className="rounded-sm bg-muted p-1">tag</p>
+                    <p className="rounded-sm bg-muted p-1">tag</p>
+                  </div>
                   <button disabled>
-                    <Calendar className="text-foreground-muted size-4" />
+                    <Calendar className="size-4 text-foreground-muted" />
                   </button>
                   <button disabled>
-                    <Target className="text-foreground-muted size-4" />
+                    <Target className="size-4 text-foreground-muted" />
                   </button>
                   <button
                     className=""
-                    onClick={() => handleDelete(tasks.indexOf(task))}
+                    onClick={() => deleteTask(tasks.indexOf(task))}
                   >
-                    <Trash2 className="text-foreground-muted size-4 hover:text-destructive" />
+                    <Trash2 className="size-4 text-foreground-muted hover:text-destructive" />
                   </button>
                   <button disabled>
-                    <Ellipsis className="text-foreground-muted size-4" />
+                    <Ellipsis className="size-4 text-foreground-muted" />
                   </button>
                 </div>
               </div>
